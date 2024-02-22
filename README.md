91e9cbb39d1ac45805fb54b8905a2dbce5462e98
rjindael <me@rjindael.xyz>
2024-02-21 21:13:03 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ShaderObfuscator
+
+Shader obfuscating tool for Aya
+
+## License
+
+Copyright (c) Kiseki 2024. All rights reserved. Not for public use.
---
+++ ShaderObfuscator/Program.cs
@@ -0,0 +1,42 @@
+using ZstdSharp;
+
+namespace ShaderObfuscator;
+
+public class Program
+{
+    private const int COMPRESSION_LEVEL = 22;
+    private const int XOR_KEY = 0xA8;
+
+    public static void Main(string[] args)
+    {
+        using var compressor = new Compressor(COMPRESSION_LEVEL);
+        string output = "";
+
+        foreach (string path in args)
+        {
+            if (path == args[^1])
+            {
+                break;
+            }
+
+            string filename = Path.GetFileNameWithoutExtension(path).Replace("shaders_", "");
+
+            byte[] bytes = File.ReadAllBytes(path);
+            byte[] compressed = compressor.Wrap(bytes).ToArray();
+
+            Array.Reverse(compressed);
+
+            for (var i = 0; i < compressed.Length; i++)
+            {
+                compressed[i] ^= XOR_KEY;
+            }
+
+            string pack = string.Join(", ", compressed.Select(b => $"0x{b:X2}"));
+            output += $"static const unsigned char {filename}[] = {{ {pack} }};\n";
+        }
+
+        output = output.Substring(0, output.Length - 1);
+
+        File.WriteAllText(args[^1], output);
+    }
+}
